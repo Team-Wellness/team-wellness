aa21ac0ae49c1d2bfe56a39e018748c6f9a8bb6a
g-tolentino <gmtolent@hawaii.edu>
2018-11-30 09:57:32 -1000

    Revert "Merge branch 'master' into glesa"
    This reverts commit 6e3a35f79ec7902a22e522966d24c7b7c748f6fa, reversing
    changes made to acf233a3d4aa3bf591deeef6abc5416e0543fd2c.

--- FlaskApp/dummy.sql
+++
@@ -1,9 +0,0 @@
-insert into patients(username, id_num, password, name, age, birthday, sex, height, weight, img) values ("patient1", 2938490, "password", "Max Z", 21, "July 6", "X", "60 in", 123, "https://66.media.tumblr.com/fc6c724a178c19c028fc33d2c19d8177/tumblr_pgo5oultCp1r9z0kqo1_640.png");
-
-insert into patients(username, id_num, password, name, age, birthday, sex, height, weight, img) values ("patient2", 9438, "password", "Edwin Z", 18, "June 17", "M", "68 in", 999, "https://66.media.tumblr.com/a488faeb262a0ae714cca91deeca65f4/tumblr_phkl0cystq1s0s8m8_540.jpg");
-
-insert into patients(username, id_num, password, name, age, birthday, sex, height, weight, img) values ("patient3", 43825, "password", "Wendy C", 22, "August 43", "F", "63 in", 676, "https://66.media.tumblr.com/ca38901911eea94f5596fb202e92f2bf/tumblr_pgp98xrmzX1rd615wo1_500.gif");
-
-insert into patients(username, id_num, password, name, age, birthday, sex, height, weight, img) values ("patient4", 43984, "password", "Matt C", 21, "March 19", "M", "69 in", 989, "https://66.media.tumblr.com/a77da79293850e89ddc89fe647843a7e/tumblr_pey3v5AnH41wjwprio2_540.jpg");
-
-insert into relationships(dr_id, patient_id, relationship_id) values (1, 2938490, 9384909482304);
